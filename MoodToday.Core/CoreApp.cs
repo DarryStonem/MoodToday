028d5f0ed615a8b8772d7c23a04ffb292143e86e
proto <proto@DESKTOP-BOR41SI>
2016-08-31 02:20:52 -0500

    Add project files.

---
+++ MoodToday.Core/CoreApp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodToday.Core
+{
+    public class CoreApp
+    {
+        public String Hola { get; set; }
+
+        public CoreApp()
+        {
+            Hola = "Hola puto";
+        }
+    }
+}
